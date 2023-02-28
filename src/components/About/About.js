631f996caecd339f0aebedc1a1af24a44f8c9606
Harinda Vishwantha <hari97vish@gmail.com>
2023-02-28 22:23:07 +0530

    Github calander Todo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
+//Todo import Github from "./Github";
+
 
 function About() {
   return (
